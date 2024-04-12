7051996ac50482dd935e8af843a460ca6b2856f2
octaviolobo <163486842+octaviolobo@users.noreply.github.com>
2024-04-11 20:30:09 -0700

    to do list

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
 //alert("Hello World")
-let jhon_doe = {
-    name:"jhon doe",
-    age: 25,
-    pretty: true, 
+let ToDo = [ir_trabalhar, college, walmart, home ];
 
-};
-let y = null;
-let x;
-const PI = 3.14159;
-console.log("person name", jhon_doe.name);
-console.log("age",jhon_doe.age)
-console.log("is he pretty?",jhon_doe.pretty)
-console.log("null example",y);
-console.log("undefined example",x);
-console.log("const example",PI);
