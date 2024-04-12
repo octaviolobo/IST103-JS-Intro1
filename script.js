f77c0d833e93a1846d1be6c882e837abdd6d9d6f
octaviolobo <163486842+octaviolobo@users.noreply.github.com>
2024-04-11 20:33:46 -0700

    console log array and object

--- script.js
+++ script.js
@@ -7,4 +7,8 @@ let backpack = {
     compartments: 5,
     waterproof: false, 
 };
-
+console.log("to do list", ToDo);
+console.log("brand:", backpack.brand);
+console.log("color:", backpack.color);
+console.log("compartments:", backpack.compartments);
+console.log("waterproof:", backpack.waterproof);
