1972a254b929315a0c6d01e5b58b59e746e6b8d1
octaviolobo <163486842+octaviolobo@users.noreply.github.com>
2024-04-11 20:28:08 -0700

    description.console.log

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ let jhon_doe = {
 let y = null;
 let x;
 const PI = 3.14159;
-console.log("person name is"+ jhon);
-console.log(y);
-console.log(x);
-console.log(PI);
+console.log("person name", jhon_doe.name);
+console.log("age",jhon_doe.age)
+console.log("is he pretty?",jhon_doe.pretty)
+console.log("null example",y);
+console.log("undefined example",x);
+console.log("const example",PI);
