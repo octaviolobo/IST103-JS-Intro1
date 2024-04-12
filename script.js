e86fee2f99abb0c040b83fbddce35d44bcfc01da
octaviolobo <163486842+octaviolobo@users.noreply.github.com>
2024-04-11 20:31:17 -0700

    object

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 //alert("Hello World")
 let ToDo = [ir_trabalhar, college, walmart, home ];
 
+let backpack = {
+    brand:"Jm engenharia",
+    color: "grey",
+    compartments: 5,
+    waterproof: false, 
+};
+
